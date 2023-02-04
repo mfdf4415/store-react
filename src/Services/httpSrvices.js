e42258b4006a3fe3f049522adc4a0b64d56fbda9
d.fathi <d.fathi@kian.digital>
2023-02-04 16:02:52 +0330

    create httpServices and set intercepors baseURL

---
+++ src/Services/httpSrvices.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.escuelajs.co/api/v1"
+
+axios.interceptors.response.use(res => {
+    console.log(res.data)
+}, error => {
+    console.error(error)
+})
+
+const http = {
+    get: axios.get,
+    post: axios.post,
+    delete: axios.delete,
+    put: axios.put
+}
+
+export default http
